38761ce8715c2423ba9a717cfabf03b70b144263
Eugene0901 <skystick0901@gmail.com>
2022-05-25 21:47:44 +0900

    Front end added

---
+++ Jinseong/front/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [state, setState] = useState([]);
+  const URL = "https://jsonplaceholder.typicode.com/posts";
+  useEffect(() => {
+axios.get(URL).then((res) => {
+  setState(res.date);
+});
+state.length > 0 && console.log(state);
+}, []);
+
+  return (
+    <div>
+      <header>박진성</header>
+    {state.map((v) => (
+       <div style={{padding: "10px", color: "blue"}}>{v.title}</div>
+     ))}
+    </div>
+  );
+}
+
+export default App;
