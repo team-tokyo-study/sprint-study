d1ba738dcc30cd4f1ec0d9906f0b6db79832084d
unknown <you@example.com>
2022-06-01 23:54:03 +0900

    Add Backend

---
+++ Jinseong/backend/newbie/src/test/java/com/example/newbie/newbie/NewbieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newbie.newbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
