0917f7c748d71fee100a685b08cc83dbbb065a58
Jinseong Park <105361779+Eugene0901@users.noreply.github.com>
2022-06-29 16:38:14 +0900

    Revert "DTO 1"

--- Jinseong/backend/newbie/src/main/java/com/example/newbie/controller/NewbieController.java
+++ Jinseong/backend/newbie/src/main/java/com/example/newbie/controller/NewbieController.java
@@ -1,11 +1,8 @@
 package com.example.newbie.controller;
 
-import com.example.newbie.dto.CreateDTO;
-import com.example.newbie.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NewbieController {
@@ -14,17 +11,5 @@ public class NewbieController {
     public String hello(Model model) {
         model.addAttribute("name", "mypage");
         return "mypage";
-    }
-        @GetMapping("/mypage")
-        public String mypage(Model model){
-            model.addAttribute("name", "mypage model");
-            return "mypage";
-        }
-    @PostMapping("/mypage/create")
-    public String createArticle(CreateDTO form) {
-        System.out.println(form.toString());
-        Article article = form.toEntity();
-        System.out.println(article.toString());
-        return "";
     }
 }
